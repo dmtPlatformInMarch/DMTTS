d2a50da240f7ff0c31b41cdd460a7a7c855889d5
kijoongkwon99 <kijoongkwon@kaist.ac.kr>
2026-01-15 12:16:17 +0900

    Ubuntu 20.04

--- src/dmtts/__init__.py
+++ src/dmtts/__init__.py
@@ -1,6 +1,6 @@
 from dmtts.app.api import TTS as DMInference
 
-__version__ = "1.0.0" # Ubuntu 22.04
-# __version__ = "1.0.1" # Ubuntu 20.04
+# __version__ = "1.0.0" # Ubuntu 22.04
+__version__ = "1.0.1" # Ubuntu 20.04
 __author__ = "Kijoong Kwon"
 __license__ = "MIT"
